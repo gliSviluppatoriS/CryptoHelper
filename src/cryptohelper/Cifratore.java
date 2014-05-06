bec54113ec84ab0305f4800044d9acd4e269617c
Lorenzo Pierini <nigni91-iper@yahoo.com>
2014-05-06 12:37:18 +0200

    aggiunto cifratore e proposta

---
+++ src/cryptohelper/Cifratore.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cryptohelper;
+
+/**
+ *
+ * @author lorenzo
+ */
+public class Cifratore {
+    public static String cifra(Mappatura map, String testo){
+        
+        return null;
+    }
+    
+    public static String decifra(Mappatura map, String testo){
+        
+        return null;
+    }
+    
+}
---
+++ src/cryptohelper/Proposta.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cryptohelper;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author lorenzo
+ */
+public class Proposta {
+    private String stato;
+    private UserInfo proponente;
+    private UserInfo partner;
+    private SistemaCifratura proposta;
+    
+    public Proposta(ResultSet queryResult){
+        
+    }
+    public Proposta(UserInfo prop, UserInfo part, SistemaCifratura sdc){
+        proponente = prop;
+        partner = part;
+        proposta = sdc;
+        stato = null;
+    }
+    
+    public static Proposta caricaAttiva(String idProp, String idPart){
+        return null;
+    }
+    
+    public void salva(){
+        
+    }
+}
